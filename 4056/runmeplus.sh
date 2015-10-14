208d4d4e0f20fb6d08190f939d7c80809a0a279b
faisalagustp <faisalagustp@Faisals-MacBook-Pro.local>
2015-10-14 18:10:32 +0700

    testing

---
+++ 4056/runmeplus.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sbatch vv1.1;
+sbatch vv1.2;
+sbatch vv1.4;
+
+sbatch vv2.1;
+sbatch vv2.2;
+sbatch vv2.4;
+
+sbatch mv1.1;
+sbatch mv1.2;
+sbatch mv1.4;
+
+sbatch mv2.1;
+sbatch mv2.2;
+sbatch mv2.4;
+
+sbatch mm1.1;
+sbatch mm1.2;
+sbatch mm1.4;
+
+sbatch mm2.1;
+sbatch mm2.2;
+sbatch mm2.4;
